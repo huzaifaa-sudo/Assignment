759bc3c8a17456da8c1c80ce6da1ed83f72b4aaf
Huzaifa Adil <huzaikiller@gmail.com>
2026-06-11 13:20:50 +0500

    Update Assigment.js

--- Assigment.js
+++ Assigment.js
@@ -3,7 +3,7 @@
 let num1 = 10;
 let num2 = 20;
 console.log(num1 + num2)
-=30
+// =30
 // Print their sum.
 
 // Exercise 2:
@@ -11,7 +11,7 @@ console.log(num1 + num2)
 a = 50
 b = 15
 console.log(a - b)
-=35
+// =35
 //Print their difference.
 
 // Exercise 3:
@@ -19,7 +19,7 @@ console.log(a - b)
 x = 12
 y = 5
 console.log(12 * 5)
-=60
+// =60
 //Print their product.
 
 // Exercise 4:
@@ -27,7 +27,7 @@ console.log(12 * 5)
 total = 100
 part = 4
 console.log(total / part)
-=25
+// =25
 //Print the division result.
 
 // Exercise 5:
@@ -37,7 +37,7 @@ english = 70
 science = 90
 totalMarks = (math+english+science)
 console.log(totalMarks)
-=240
+// =240
 // Calculate and print the total marks.
 
 // Exercise 6:
@@ -47,7 +47,7 @@ english = 70
 science = 90
 average = totalMarks / 3
 console.log(average)
-=80
+// =80
 // calculate and print the average marks.
 
 // Exercise 7:
@@ -56,7 +56,7 @@ length = 10
 width = 5
 area = length * width
 console.log(area)
-=50
+// =50
 // Calculate and print the area of a rectangle.
 
 // Exercise 8:
@@ -64,7 +64,7 @@ console.log(area)
 currentAge = 20
 age = (currentAge + 5)
 console.log(age)
-=25
+// =25
 // Calculate and print the age after 5 years.
 
 // Exercise 9:
@@ -72,7 +72,7 @@ console.log(age)
 weeklyPocketMoney = 500
 pocketMoney = (weeklyPocketMoney * 4)
 console.log(pocketMoney)
-=2000
+// =2000
 // Calculate and print the pocket money for 4 weeks.
 
 // Exercise 10:
@@ -82,7 +82,7 @@ noteBookPrice= 120
 eraserPrice= 20
 shoppingBill = (penPrice+noteBookPrice+eraserPrice)
 console.log(shoppingBill)
-=170
+// =170
 // Calculate and print the total shopping bill.
 
 // Exercise 11:
@@ -91,7 +91,7 @@ basicSalary = 30000
 bonus = 5000
 total =(basicSalary+bonus)
 console.log(total)
-=35000
+// =35000
 // Calculate and print the total salary.
 
 // Exercise 12:
@@ -101,7 +101,7 @@ match2 = 60
 match3 = 80
 totalCricketScore = (match1+match2+match3)
 console.log(totalCricketScore)
-=185
+// =185
 // Calculate and print the total cricket score.
 
 // Challenge Exercise:
