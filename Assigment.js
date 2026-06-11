d4aa9fe095c1779cee347f1f319b8e33c6407c9e
Huzaifa Adil <huzaikiller@gmail.com>
2026-06-11 13:19:20 +0500

    Update Assigment.js

--- Assigment.js
+++ Assigment.js
@@ -1,66 +1,117 @@
-// let num1 = 10;
-// let num2 = 20;
-// console.log(num1 + num2)
+// Exercise 1:
+//Create two variables:
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2)
+=30
+// Print their sum.
 
-// a = 50
-// b = 15
-// console.log(a - b)
+// Exercise 2:
+//Create two variables:
+a = 50
+b = 15
+console.log(a - b)
+=35
+//Print their difference.
 
-// x = 12
-// y = 5
-// console.log(12 * 5)
+// Exercise 3:
+//Create two variables:
+x = 12
+y = 5
+console.log(12 * 5)
+=60
+//Print their product.
 
-// total = 100
-// part = 4
-// console.log(total / part)
+// Exercise 4:
+//Create two variables:
+total = 100
+part = 4
+console.log(total / part)
+=25
+//Print the division result.
 
-// math = 80
-// english = 70
-// science = 90
-// totalMarks = (math+english+science)
-// console.log(totalMarks)
+// Exercise 5:
 
-// average = totalMarks / 3
-// console.log(average)
+math = 80
+english = 70
+science = 90
+totalMarks = (math+english+science)
+console.log(totalMarks)
+=240
+// Calculate and print the total marks.
 
-// length = 10
-// width = 5
-// area = length * widthl=
-// console.log(area)
+// Exercise 6:
 
-// currentAge = 20
-// age = (currentAge + 5)
-// console.log(age)
+math = 80
+english = 70
+science = 90
+average = totalMarks / 3
+console.log(average)
+=80
+// calculate and print the average marks.
 
-// weeklyPocketMoney = 500
-// pocketMoney = (weeklyPocketMoney * 4)
-// console.log(pocketMoney)
+// Exercise 7:
 
-// penPrice= 30
-// noteBookPrice= 120
-// eraserPrice= 20
-// shoppingBill = (penPrice+noteBookPrice+eraserPrice)
-// console.log(shoppingBill)
+length = 10
+width = 5
+area = length * width
+console.log(area)
+=50
+// Calculate and print the area of a rectangle.
 
-// basicSalary = 30000
-// bonus = 5000
-// total =(basicSalary+bonus)
-// console.log(total)
+// Exercise 8:
 
-// match1 = 45
-// match2 = 60
-// match3 = 80
-// totalCricketScore = (match1+match2+match3)
-// console.log(totalCricketScore)
+currentAge = 20
+age = (currentAge + 5)
+console.log(age)
+=25
+// Calculate and print the age after 5 years.
 
-// let a = 10;
+// Exercise 9:
 
-// let b = "5";
+weeklyPocketMoney = 500
+pocketMoney = (weeklyPocketMoney * 4)
+console.log(pocketMoney)
+=2000
+// Calculate and print the pocket money for 4 weeks.
+
+// Exercise 10:
+
+penPrice= 30
+noteBookPrice= 120
+eraserPrice= 20
+shoppingBill = (penPrice+noteBookPrice+eraserPrice)
+console.log(shoppingBill)
+=170
+// Calculate and print the total shopping bill.
+
+// Exercise 11:
+
+basicSalary = 30000
+bonus = 5000
+total =(basicSalary+bonus)
+console.log(total)
+=35000
+// Calculate and print the total salary.
+
+// Exercise 12:
+
+match1 = 45
+match2 = 60
+match3 = 80
+totalCricketScore = (match1+match2+match3)
+console.log(totalCricketScore)
+=185
+// Calculate and print the total cricket score.
+
+// Challenge Exercise:
 
 let a = 10
 let b = "5"
 
-console.log(a + b);105
-console.log(a - b);5
-console.log(a * b);50
-console.log(a / b);2
+console.log(a + b)=105
+console.log(a - b)=5
+console.log(a * b)=50
+console.log(a / b)=2
+
+// Predict the output before running:
