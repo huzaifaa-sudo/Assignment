da33c21006ed5e353943e38ee021007221fda415
Huzaifa Adil <huzaikiller@gmail.com>
2026-06-11 13:17:16 +0500

    Add files via upload

---
+++ Assigment.js
@@ -0,0 +1,66 @@
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1 + num2)
+
+// a = 50
+// b = 15
+// console.log(a - b)
+
+// x = 12
+// y = 5
+// console.log(12 * 5)
+
+// total = 100
+// part = 4
+// console.log(total / part)
+
+// math = 80
+// english = 70
+// science = 90
+// totalMarks = (math+english+science)
+// console.log(totalMarks)
+
+// average = totalMarks / 3
+// console.log(average)
+
+// length = 10
+// width = 5
+// area = length * widthl=
+// console.log(area)
+
+// currentAge = 20
+// age = (currentAge + 5)
+// console.log(age)
+
+// weeklyPocketMoney = 500
+// pocketMoney = (weeklyPocketMoney * 4)
+// console.log(pocketMoney)
+
+// penPrice= 30
+// noteBookPrice= 120
+// eraserPrice= 20
+// shoppingBill = (penPrice+noteBookPrice+eraserPrice)
+// console.log(shoppingBill)
+
+// basicSalary = 30000
+// bonus = 5000
+// total =(basicSalary+bonus)
+// console.log(total)
+
+// match1 = 45
+// match2 = 60
+// match3 = 80
+// totalCricketScore = (match1+match2+match3)
+// console.log(totalCricketScore)
+
+// let a = 10;
+
+// let b = "5";
+
+let a = 10
+let b = "5"
+
+console.log(a + b);105
+console.log(a - b);5
+console.log(a * b);50
+console.log(a / b);2
